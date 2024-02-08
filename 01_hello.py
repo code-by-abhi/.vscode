6248d5833445929f9e1ad404f2871cbfafe38a91
abhi <abhiiii876@gmail.com>
2024-02-08 22:03:23 +0530

    starting programming

---
+++ 01_hello.py
@@ -0,0 +1,4 @@
+'''Author: abhi~SINGLE LINE COMMENT 
+Licenced to: abhi company CH.1'''
+import os
+print("hello world") 
---
+++ 02_pr_01_twinkle.py
@@ -0,0 +1,8 @@
+'''this is a solution
+of problem 1 ofcode with harry
+'''
+# this is also a comment like the above line but its a sinlge line comment 
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.''')
